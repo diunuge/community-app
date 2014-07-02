239be80264353fb4233e474ebe203f7b4c14b7e0
Diunuge <diunuge.10@cse.mrt.ac.lk>
2014-07-02 21:55:18 +0530

    Minor changes to client import- Added the redirection

--- app/scripts/controllers/client/ImportClientController.js
+++ app/scripts/controllers/client/ImportClientController.js
@@ -30,74 +30,11 @@
                 });*/
             };
             
-            /*scope.serializeData = function ( data ) {
-            	 
-                // If this is not an object, defer to native stringification.
-                if ( ! angular.isObject( data ) ) {
-
-                    return( ( data == null ) ? "" : data.toString() );
-
-                }
-
-                var buffer = [];
-
-                // Serialize each key in the object.
-                for ( var name in data ) {
-
-                    if ( ! data.hasOwnProperty( name ) ) {
-
-                        continue;
-
-                    }
-
-                    var value = data[ name ];
-
-                    buffer.push(
-                        encodeURIComponent( name ) +
-                        "=" +
-                        encodeURIComponent( ( value == null ) ? "" : value )
-                    );
-
-                }
-
-                // Serialize the buffer and clean it up for transportation.
-                var source = buffer
-                    .join( "&" )
-                    .replace( /%20/g, "+" )
-                ;
-
-                return( source );
-
-            };*/
-            
             scope.onFileSelect = function ($files) {
                 scope.fileToUpload = $files[0];
             };
             
             scope.importClients = function () {
-
-            	/*this.formDataPost.clientType = scope.clientType.index;
-            	var clientType = scope.clientType.index;
-            	//var file = scope.excelFile;
-                //console.log('file is ' + JSON.stringify(file));
-                //var uploadUrl = "http://localhost:8080/mifosng-provider/api/v1/clients/import/0";
-                //FileUploadService.uploadFileToUrl(file, uploadUrl);
-                
-                
-            	//this.formDataPost.excelFile = scope.excelFile;
-            	//this.formDataPost.form = new FormData();
-            	//this.formDataPost.form.append('file', scope.excelFile);
-            	
-            	//resourceFactory.clientImportResource.importClients(this.formDataPost, function (data) {
-            		//location.path('/viewclient/' + 0);
-                //});
-                var fd = new FormData();
-                fd.append('file', scope.excelFile);
-                this.formDataPost.fd = fd;
-                //http.defaults.useXDomain = true;
-                resourceFactory.clientImportResource.importClients( fd, function (data) {
-                    
-                });*/
             	
             	this.formDataPost.clientType = scope.clientType.index;
             	
@@ -107,10 +44,10 @@
                     file: scope.fileToUpload
                 }).then(function (data) {
                         // to fix IE not refreshing the model
-                        //if (!scope.$$phase) {
-                        //    scope.$apply();
-                        //}
-                        //location.path('/viewclient/' + scope.clientId);
+                        if (!scope.$$phase) {
+                            scope.$apply();
+                        }
+                        location.path('/viewclient/');
                     });
             };
         }
